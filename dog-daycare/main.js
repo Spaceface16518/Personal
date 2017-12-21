f17e3e5d1552af0485fc99e2f74374bc7aa11720
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2017-12-20 19:37:32 -0600

    commit

--- dog-daycare/main.js
+++ dog-daycare/main.js
@@ -1,7 +1,8 @@
 class Dog {
-  constructor(name, entryCount) {
-    this.name = name;
-    this.isAtDaycare;
-    this.entryCount = 0;
+  constructor(name) {
+    this._name = name; // string value
+    this._isAtDaycare; // boolean value
+    this._entryCount = 0; // integer value
   }
+  // TODO: add getter and setter methods
 }
