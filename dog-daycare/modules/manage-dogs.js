35be0577b1c36f24cbaa97f052ebdaee1d532533
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2017-12-20 22:30:41 -0600

    Beautify

--- dog-daycare/modules/manage-dogs.js
+++ dog-daycare/modules/manage-dogs.js
@@ -1,13 +1,15 @@
 var dogList = [];
+
 function addDog(name) {
   const Dog = new Dog(name);
   console.log(`Dog ${Dog.name} created and added to dog list`);
 }
+
 function removeDog(name) {
-let index = dogList.indexOf[name];
-if (index >= 0) {
-  dogList.splice(index, 1)
-} else {
-  console.log('Invalid index: the list number of the dog you were trying to find was not greater than or equal to zero')
-}
+  let index = dogList.indexOf[name];
+  if (index >= 0) {
+    dogList.splice(index, 1)
+  } else {
+    console.log('Invalid index: the list number of the dog you were trying to find was not greater than or equal to zero')
+  }
 }
