b6d25fcbb9082b4913dec6a365b608ebd090ee32
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-01-14 19:27:07 -0600

    Commit

--- javascript/modules/module-test/main-tester.js
+++ javascript/modules/module-test/main-tester.js
@@ -1,4 +1,4 @@
-import {write} from "https: //gist.github.com/Spaceface16518/f91d67bf0745cead14dc6f81d015e187.js"
+import { write } from "https://raw.githubusercontent.com/Spaceface16518/Personal/master/javascript/modules/Write.js"
 $(document).ready(function() {
 () => {
   write("Hello, world!")
