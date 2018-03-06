ce955d887b2be555bf8f64cdfaf4c36d1c7e84b2
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-03-06 11:13:56 -0600

    Upgrade perl program

--- by-language/perl/BegPerl/lessons/sum-args.pl
+++ by-language/perl/BegPerl/lessons/sum-args.pl
@@ -4,7 +4,10 @@ use warnings;
 use Scalar::Util qw(looks_like_number);
 
 # PROGRAM
-print "Enter some arguments separated by spaces: ";
+print "\nWelcome to Amrit's accumulative argument addition apparatus. Enter an argument and press enter to submit it. Enter a blank line to sum all of the arguments you entered. Press Crtl+C to quit the program.\n";
+while(42)
+{
+  print "\n";
 our @arguments = ();
 get_args();
 our $sum = sum_args(@arguments);
@@ -30,7 +33,6 @@ sub print_args
 
 sub get_args
 {
-  print "Enter some arguments, or enter a blank line to sum\n";
   my $continue = 1;
 
   while ($continue == 1)
@@ -62,4 +64,5 @@ sub sum_args
     $total += $_ ;
   }
   return $total;
+}
 }
